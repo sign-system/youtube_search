ae1632cd68a3c900ccd49dfac1d17df945798055
eric bel <ericbelecov@gmail.com>
2022-06-04 04:20:07 +0400

    favourites_icon done

--- src/components/Main/Search/SearchForm/SearchForm.jsx
+++ src/components/Main/Search/SearchForm/SearchForm.jsx
@@ -1,17 +1,22 @@
 import s from "./SearchForm.module.css";
+import { AiOutlineHeart } from "react-icons/ai";
 
 const SearchForm = ({ handleSubmit, handleChange }) => {
   return (
     <div className={s.search}>
       <div className={s.search_title}>Поиск видео</div>
+
       <form className={s.search_form} onSubmit={handleSubmit}>
-        <input
-          className={s.search_input}
-          onChange={handleChange}
-          type="search"
-          name="video-search"
-          placeholder="Введите запрос"
-        />
+        <div className={s.input_and_favicon}>
+          <input
+            className={s.search_input}
+            onChange={handleChange}
+            type="text"
+            name="video-search"
+            placeholder="Введите запрос"
+          />
+          <AiOutlineHeart className={s.favourites_icon} />
+        </div>
         <button className={s.search_button}>Найти</button>
       </form>
     </div>
